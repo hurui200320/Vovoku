884eb6bc2a46a06c7de3a08011a9270120df1e5a
天空Blond <hurui200320@skyblond.info>
2021-05-08 14:46:56 +0800

    Done with client admin apis
    Signed-off-by: 天空Blond <hurui200320@skyblond.info>

--- frontend/src/main/kotlin/info/skyblond/vovoku/frontend/Main.kt
+++ frontend/src/main/kotlin/info/skyblond/vovoku/frontend/Main.kt
@@ -2,9 +2,8 @@ package info.skyblond.vovoku.frontend
 
 import info.skyblond.vovoku.commons.CryptoUtil
 import info.skyblond.vovoku.commons.JacksonJsonUtil
-import info.skyblond.vovoku.commons.models.AdminUserRequest
-import info.skyblond.vovoku.commons.models.CRUD
 import info.skyblond.vovoku.commons.models.DatabaseUserPojo
+import info.skyblond.vovoku.commons.models.Page
 import info.skyblond.vovoku.frontend.api.admin.AdminApiClient
 import okhttp3.OkHttpClient
 import java.io.File
@@ -15,13 +14,47 @@ fun main() {
         JacksonJsonUtil.readFromFile<CryptoUtil.RSAKeySpec>(File("D:\\Git\\github\\Vovoku\\backend\\privateKey.json"))
     val adminApiClient = AdminApiClient(httpClient, "http://127.0.0.1:7000/admin", privateKey.restore())
 
+    // print old
     println(
         adminApiClient.queryUser(
-            AdminUserRequest(
-                DatabaseUserPojo(null, "%hu%", null),
-                CRUD.READ,
-                null
-            )
+            DatabaseUserPojo(null, "%hu%"),
+            Page(1, 20)
+        )
+    )
+
+    // add new, should success
+    println(
+        adminApiClient.addUser(
+            DatabaseUserPojo(null, "hurui200320", CryptoUtil.md5("Some password"))
+        )
+    )
+
+    // add new, should failed
+    println(
+        adminApiClient.addUser(
+            DatabaseUserPojo(null, "hurui200320", CryptoUtil.md5("Some1 password"))
+        )
+    )
+
+    // print new
+    println(
+        adminApiClient.queryUser(
+            DatabaseUserPojo(null, "%hu%"),
+            Page(1, 20)
+        )
+    )
+
+    // delete new
+    println(
+        adminApiClient.deleteUser(
+            DatabaseUserPojo(null, "hurui200%")
+        )
+    )
+
+    println(
+        adminApiClient.queryUser(
+            DatabaseUserPojo(null, "%hu%"),
+            Page(1, 20)
         )
     )
 }
--- frontend/src/main/kotlin/info/skyblond/vovoku/frontend/api/admin/AdminApiClient.kt
+++ frontend/src/main/kotlin/info/skyblond/vovoku/frontend/api/admin/AdminApiClient.kt
@@ -2,8 +2,7 @@ package info.skyblond.vovoku.frontend.api.admin
 
 import info.skyblond.vovoku.commons.CryptoUtil
 import info.skyblond.vovoku.commons.JacksonJsonUtil
-import info.skyblond.vovoku.commons.models.AdminUserRequest
-import info.skyblond.vovoku.commons.models.DatabaseUserPojo
+import info.skyblond.vovoku.commons.models.*
 import info.skyblond.vovoku.frontend.api.jsonMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -19,9 +18,12 @@ class AdminApiClient(
     private val logger = LoggerFactory.getLogger(AdminUserRequest::class.java)
     private val urlPrefix: String = _urlPrefix.removeSuffix("/")
 
+    private val usersEndPoint = "users"
+    private val picturesEndPoint = "pictures"
+    private val modelsEndPoint = "models"
+
     private fun postJson(url: String, json: String): Pair<Int, String> {
         val requestBody = json.toRequestBody(jsonMediaType)
-        logger.info("Posting to $url")
         val request = Request.Builder()
             .url(url)
             .post(requestBody)
@@ -30,15 +32,93 @@ class AdminApiClient(
         httpClient.newCall(request).execute()
             .use { response ->
                 return response.code to (response.body?.string()
-                    ?.let { CryptoUtil.decryptWithPrivateKey(it, privateKey) } ?: "")
+                    ?.let {
+                        if (response.code == 200)
+                            CryptoUtil.decryptWithPrivateKey(it, privateKey)
+                        else
+                            it
+                    } ?: "")
             }
     }
 
-    fun queryUser(requestBody: AdminUserRequest): List<DatabaseUserPojo> {
-        val json = JacksonJsonUtil.objectToJson(requestBody)
-        val response = postJson("$urlPrefix/users", json)
-        require(response.first == 200)
-        logger.info("Response: $response")
-        return JacksonJsonUtil.jsonToObject(response.second)
+    /**
+     * Make query and parse the result
+     * */
+    private fun <T> query(body: Any, endPoint: String): List<T> {
+        val json = JacksonJsonUtil.objectToJson(body)
+        val response = postJson("$urlPrefix/$endPoint", json)
+        if (response.first == 200) {
+            return JacksonJsonUtil.jsonToObject(response.second)
+        } else {
+            logger.warn("Server response '${response.first}': ${response.second}")
+            throw IllegalStateException(response.second)
+        }
     }
+
+    /**
+     * Send request and expect to be HTTP 200
+     * */
+    private fun doRequest(body: Any, endPoint: String): Boolean{
+        val json = JacksonJsonUtil.objectToJson(body)
+        val response = postJson("$urlPrefix/$String", json)
+        if (response.first != 200) {
+            logger.error("Server response '${response.first}': ${response.second}")
+        }
+        return response.first == 200
+    }
+
+
+    fun queryUser(pojo: DatabaseUserPojo, page: Page): List<DatabaseUserPojo> {
+        val requestBody = AdminUserRequest(pojo, CRUD.READ, page)
+        return query(requestBody, usersEndPoint)
+    }
+
+    fun addUser(pojo: DatabaseUserPojo): Boolean {
+        val requestBody = AdminUserRequest(pojo, CRUD.CREATE, null)
+        return doRequest(requestBody, usersEndPoint)
+    }
+
+    fun updateUser(pojo: DatabaseUserPojo): Boolean {
+        val requestBody = AdminUserRequest(pojo, CRUD.UPDATE, null)
+        return doRequest(requestBody, usersEndPoint)
+    }
+
+    fun deleteUser(pojo: DatabaseUserPojo): Boolean {
+        val requestBody = AdminUserRequest(pojo, CRUD.DELETE, null)
+        return doRequest(requestBody, usersEndPoint)
+    }
+
+
+    fun queryPicture(pojo: DatabasePictureTagPojo, page: Page): List<DatabasePictureTagPojo> {
+        val requestBody = AdminPictureTagRequest(pojo, CRUD.READ, page)
+        return query(requestBody, picturesEndPoint)
+    }
+
+    fun updatePicture(pojo: DatabaseUserPojo): Boolean {
+        val requestBody = AdminUserRequest(pojo, CRUD.UPDATE, null)
+        return doRequest(requestBody, picturesEndPoint)
+    }
+
+    fun deletePicture(pojo: DatabaseUserPojo): Boolean {
+        val requestBody = AdminUserRequest(pojo, CRUD.DELETE, null)
+        return doRequest(requestBody, picturesEndPoint)
+    }
+
+
+    fun queryModel(pojo: DatabaseModelInfoPojo, page: Page): List<DatabaseModelInfoPojo> {
+        val requestBody = AdminModelRequest(pojo, CRUD.READ, page)
+        return query(requestBody, modelsEndPoint)
+    }
+
+    fun updateModel(pojo: DatabaseModelInfoPojo): Boolean {
+        val requestBody = AdminModelRequest(pojo, CRUD.UPDATE, null)
+        return doRequest(requestBody, modelsEndPoint)
+    }
+
+    fun deleteModel(pojo: DatabaseModelInfoPojo): Boolean {
+        val requestBody = AdminModelRequest(pojo, CRUD.DELETE, null)
+        return doRequest(requestBody, modelsEndPoint)
+    }
+
+
 }
